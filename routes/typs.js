d82f854b34528096f08b52a5d170b8deadfffd57
sendamdb <sendameddeb@outlook.fr>
2022-12-22 16:53:56 +0100

    add POST typs route

--- routes/typs.js
+++ routes/typs.js
@@ -1,83 +1,73 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-require('../models/connection');
-const Typ = require('../models/typs');
-const { checkBody } = require('../modules/checkBody');
-const cloudinary = require('cloudinary').v2;
-const fs = require('fs');
-const uniqid = require('uniqid');
+require("../models/connection");
+const Typ = require("../models/typs");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
+const uniqid = require("uniqid");
+const { default: user } = require("../../cityps_front/reducers/user");
 
 // ---------------------------------------------- route user/signup ----------------------------------------------
-router.post('/', async (req, res) => {
-  
-    // Vérifier que les champs de saisie ont été remplis correctement  
-    /*if (!checkBody(req.body, ['title','content', 'pictures', 'email', 'password', 'categories'])) {
+router.post("/", async (req, res) => {
+  // Vérifier que les champs de saisie ont été remplis correctement
+  /*if (!checkBody(req.body, ['title','content', 'pictures', 'email', 'password', 'categories'])) {
       res.json({ result: false, error: 'Missing or empty fields' });
       return;
     }*/
-  
 
-    const {name, firstname, username, email, password, categories} = req.body;
-    const categoriesArray = categories.split(',')
-  
-    //----------------------------------cloudinary post 3 photos------------------------------------------------------------------//
-    let photo = '../assets/avatar.png' // photo par défaut-> ?
-  
-    if (req.files !== null) {
+  const { title, city, content, author, coordinates, categories } = req.body;
+  const categoriesArray = categories.split(",");
+
+  //----------------------------------cloudinary post 3 photos------------------------------------------------------------------//
+  // let photo = "../assets/avatar.png"; // photo par défaut-> ?
+  const photos = [];
+  if (req.files !== null) {
+    for (let i = 0; i < req.files.length; i++) {
       const photoPath = `./tmp/${uniqid()}.jpg`;
-      const resultMove = await req.files.photoFromFront.mv(photoPath);
-      if(!resultMove) {
-        const resultCloudinary = 
-         await cloudinary.uploader.upload(photoPath);
-        photo = resultCloudinary.secure_url
+      const resultMove = await req.files[i].mv(photoPath);
+      if (!resultMove) {
+        const resultCloudinary = await cloudinary.uploader.upload(photoPath);
+        photos.push(resultCloudinary.secure_url);
       } else {
         // Erreur à traiter
       }
       fs.unlinkSync(photoPath);
     }
+  }
 
-    //- enregistrement dans la BDD
-  
-        Typ.findOne({content: content}).then(data => {
-            const votePlus = 0;
-            const voteMinus = 0;
-            if (data === null) {
-              const newTyp = new Typ({
-                title: title,
-                city : city,
-                content : content,
-                votePlus : votePlus,
-                voteMinus : voteMinus,
-                date : Date.now(),
-                pictures : pictures,
-                author : user_Id,
-                category : category,
-                coordinates : coordinates,
-              });
-        
-              newTyp.save().then(dataTyp => {
-                res.json({result: true, data: dataTyp })
-              });
-            } else {
-              // Texte descriptif déjà existant
-              res.json({result : false, error: 'Text already exists'});
-            }
-          })
-        })
+  //- enregistrement dans la BDD
 
+  User.findOne({ token: author }).then((data) => {
+    const newTyp = new Typ({
+      title,
+      city,
+      content,
+      date: Date.now(),
+      pictures,
+      author: data._id,
+      category: categoriesArray,
+      coordinates,
+    });
 
-  // HOMESCREEN : Créer une route GET "existingTipsbyCity" 
-  // (qui affiche tous les tips dispo sur l'appli toutes categories confondues pour cette ville)
-  
-  router.get("/:city", (req, res) => {
-  Typ.find({ city: req.params.city }).populate('author').then(data => {
-    console.log(data)
-    res.json({ result: true, city: data });
+    newTyp.save().then((dataTyp) => {
+      res.json({ result: true, data: dataTyp });
+    });
   });
-  });
+});
 
-  
+// HOMESCREEN : Créer une route GET "existingTipsbyCity"
+// (qui affiche tous les tips dispo sur l'appli toutes categories confondues pour cette ville)
 
+router.get("/:city", (req, res) => {
+  Typ.find({ city: req.params.city })
+    .populate("author")
+    .then((data) => {
+      console.log(data);
+      res.json({ result: true, city: data });
+    });
+});
 
 module.exports = router;
